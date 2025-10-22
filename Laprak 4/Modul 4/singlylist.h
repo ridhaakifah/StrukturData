49c31aa1bbca9324058bf97577e0977c0eee2f2f
Ridha Akifah <ridhaakifah2@gmail.com>
2025-10-22 23:52:20 +0700

    Add files via upload

---
+++ Laprak 4/Modul 4/singlylist.h
@@ -0,0 +1,27 @@
+#ifndef SINGLYLIST_H_INCLUDED
+#define SINGLYLIST_H_INCLUDED
+
+#include <iostream>
+
+#define Nil NULL
+
+typedef int infotype;
+typedef struct ElmList *address;
+
+struct ElmList {
+    infotype info;
+    address next;
+};
+
+struct List {
+    address First;
+};
+
+void CreateList(List &L);
+address alokasi(infotype x);
+void dealokasi(address &P);
+void insertFirst(List &L, address P);
+void insertLast(List &L, address P);
+void printInfo(List L);
+
+#endif
